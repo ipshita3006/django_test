2fa4807082d882bc2f65c85020c8030291f390ae
“ipshita” <ipshita.nandan@gyanmatrix.com>
2022-06-02 18:17:37 -0400

    Testing

--- report_test/report/models.py
+++ report_test/report/models.py
@@ -8,3 +8,18 @@ class User(models.Model):
     is_active = models.BooleanField(default=True)
 
 
+class Policy(models.Model):
+    user_id = models.OneToOneField(User, related_name="policy", on_delete=models.CASCADE)
+    hash_id = models.CharField(unique=True, null=False, max_length=10)
+    start_date = models.DateField(auto_now=True)
+    state = models.CharField(max_length=20)
+
+
+class Page(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Visit(models.Model):
+    policy_id = models.ForeignKey(Policy, related_name="visits", on_delete=models.CASCADE)
+    page_id = models.ForeignKey(Page, related_name="visits", on_delete=models.CASCADE)
+
